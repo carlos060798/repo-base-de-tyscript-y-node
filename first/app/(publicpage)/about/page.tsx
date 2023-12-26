5dcaedfc46bfd1b5f967828003b7a93fd908d402
carlos060798 <daniloangaritagarcia@gmail.com>
2023-12-26 08:28:44 -0500

    version 1

--- first/app/(publicpage)/about/page.tsx
+++ first/app/(publicpage)/about/page.tsx
@@ -1,22 +1,14 @@
+import { Metadata } from "next";
 
-
-import { Metadata } from "next"
-
-export const meta :Metadata = {
-
-    title: "About Page",
-    description: "About Page",
-    
-
-}
-
-
-export default function  About(){
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold text-center">About</h1>
-     </main>
-    );
+export const meta: Metadata = {
+  title: "About Page",
+  description: "About Page",
 };
 
-
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold text-center">About</h1>
+    </main>
+  );
+}
